41bb8e024905bdf5c5d2b526bf3b11866fd61c11
KemballP <155593325+KemballP@users.noreply.github.com>
2024-03-18 20:08:14 -0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RegistrationSystem.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string? StudentNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        // Navigation Property
+        [JsonIgnore]
+        public ICollection<Registration>? Registrations { get; set; }
+    }
+}
---
+++ StudentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistrationSystem.Models
+{
+    public class StudentRequest
+    {
+        public string? StudentNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
